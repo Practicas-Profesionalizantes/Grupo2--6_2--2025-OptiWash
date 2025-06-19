80b3bf3a6dc1429aa8b3812734264e920d8977a4
Jairo <168127511+Jairo-Carbajal@users.noreply.github.com>
2025-06-19 17:37:38 -0300

    Add files via upload

--- src - frontend/paginas/home.jsx
+++ src - frontend/paginas/home.jsx
@@ -1,23 +1,27 @@
-import Nav from "../componentes/navbar";
 import "../style/Home.css"
+import inventario from "../img/inventario/inventario.png";
+import asistencias from "../img/inventario/asistencias.png";
+import autos_lavados from "../img/inventario/autos_lavados.png";
 
 function Home (){
     return(<>
         <body>
-            <h1>Home</h1>
+            
             <div className="tarjetas">
-                <div className="tarjeta">
-                    INVENTARIO
+                <div className="tarjeta" id="t1">
+                    <h2>INVENTARIO</h2>
+                    <img src={inventario} alt=""/>
                 </div>
-                <div className="tarjeta">
-                    ASISTENCIAS
+                <div className="tarjeta" id="t2">
+                    <h2>ASISTENCIAS</h2>
+                    <img src={asistencias} alt=""/>
                 </div>
-                <div className="tarjeta">
-                    REGISTRO DE AUTOS
+                <div className="tarjeta" id="t3">
+                    <h2>REGISTROS DE AUTOS</h2>
+                    <img src={autos_lavados} alt=""/>
                 </div>
-                <div className="navbar">
 
-                </div>
+
             </div>
             
         </body>
