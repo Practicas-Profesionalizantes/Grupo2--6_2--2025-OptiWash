c16ba2a4887592f465f65b325395f79f76307458
Jairo <168127511+Jairo-Carbajal@users.noreply.github.com>
2025-06-27 17:45:21 -0300

    home esta finalizada
    solo falta añadir la navbar

--- src - frontend/paginas/home.jsx
+++ src - frontend/paginas/home.jsx
@@ -1,41 +1,34 @@
 import "../style/Home.css"
 import inventario from "../img/inventario/inventario.png";
-
 import asistencias from "../img/inventario/asistencias.png";
 import autos_lavados from "../img/inventario/autos_lavados.png";
+import { Link } from "react-router-dom";
 
 function Home (){
     return(<>
         <body>
-            
             <div className="tarjetas">
-                <div className="tarjeta" id="t1">
-                    <h2>INVENTARIO</h2>
-                    <img src={inventario} alt=""/>
-                </div>
-                <div className="tarjeta" id="t2">
-                    <h2>ASISTENCIAS</h2>
-                    <img src={asistencias} alt=""/>
-                </div>
-                <div className="tarjeta" id="t3">
-                    <h2>REGISTROS DE AUTOS</h2>
-                    <img src={autos_lavados} alt=""/>
-                <div className="tarjeta">
-                    <h2>INVENTARIO</h2>
-                    <img src={inventario} alt=""/>
-                </div>
-                <div className="tarjeta">
-                    <h2>ASISTENCIAS</h2>
-                </div>
-                <div className="tarjeta">
-                    <h2>REGISTRO DE AUTOS LAVADOS</h2>
-                </div>
-
-
+                <Link to="/inventario" style={{textDecoration:"none", color:"black"}}>
+                    <div className="tarjeta" id="t1">
+                        <h2>INVENTARIO</h2>
+                        <img src={inventario} alt=""/>
+                    </div>
+                </Link>
+                <Link to="/asistencia" style={{textDecoration:"none", color:"black"}}>
+                    <div className="tarjeta" id="t2">
+                        <h2>ASISTENCIAS</h2>
+                        <img src={asistencias} alt=""/>
+                    </div>
+                </Link>
+                <Link to="/registro_autos" style={{textDecoration:"none", color:"black"}}>
+                    <div className="tarjeta" id="t3">
+                        <h2>REGISTROS DE AUTOS</h2>
+                        <img src={autos_lavados} alt=""/>
+                    </div>
+                </Link>
             </div>
-            
         </body>
     </>)
-}
+};
 
 export default Home;
