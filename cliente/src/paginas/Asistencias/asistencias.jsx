fa12cdfb3ccc949a4a8ef2ed02a26d3ad767fe56
Juanchambi321 <juan.1chambiet32@gmail.com>
2025-11-01 00:10:25 -0300

    tocando detalles de css

--- cliente/src/paginas/Asistencias/asistencias.jsx
+++ cliente/src/paginas/Asistencias/asistencias.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./asistencia.module.css";
-
 import imgPresente from "../../assets/asistencia/presente.png";
 import imgAusente from "../../assets/asistencia/ausente.png";
 import imgTarde from "../../assets/asistencia/tarde.png";
